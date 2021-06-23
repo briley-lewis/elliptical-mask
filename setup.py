b6c813aae4aac1f0af7d01a56f2cc3ce7ec55167
Briley Lewis <blewis@New-MacBook.local>
2021-06-23 16:18:49 -0700

    made setup.py fancier

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from setuptools import setup, find_packages
+
+###taken from orbitize!
+def get_requires():
+    reqs = []
+    for line in open('requirements.txt', 'r').readlines():
+        reqs.append(line)
+    return reqs
+
 setup(
 name=”elliptical-mask”,
 version=”0.1”,
-packages=find_packages()
+description="makes arbitrary elliptical, circular, and annular binary masks for image processing",
+url='https://github.com/briley-lewis/elliptical-mask',
+license='MIT',
+packages=find_packages(),
+install_requires=get_requires()
 )
 
