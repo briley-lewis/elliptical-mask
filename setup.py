3729ed4499821bdc46d154eeb23e45222f4e4084
Briley L Lewis <briley-lewis@users.noreply.github.com>
2021-06-24 11:50:25 -0700

    Update setup.py
    new name for pypi!

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_requires():
     return reqs
 
 setup(
-name='masks',
+name='imgmask',
 version='0.1',
 description='makes arbitrary elliptical, circular, and annular binary masks for image processing',
 url='https://github.com/briley-lewis/elliptical-mask',
